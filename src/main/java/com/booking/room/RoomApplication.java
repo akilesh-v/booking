f5bb7fec45d504c969a22db81f77e2a05eaf1256
Akilesh <akilesh.v@gofrugal.com>
2021-10-29 23:37:53 +0530

    [AK] Swagger is enabled

--- src/main/java/com/booking/room/RoomApplication.java
+++ src/main/java/com/booking/room/RoomApplication.java
@@ -2,8 +2,10 @@ package com.booking.room;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class RoomApplication {
 
 	public static void main(String[] args) {
